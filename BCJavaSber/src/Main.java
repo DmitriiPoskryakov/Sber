5a072edd8e9a00dea29d3ad4da5b4a3bc1a58762
DmitriiPoskryakov <109706149+DmitriiPoskryakov@users.noreply.github.com>
2023-01-20 00:10:24 +0500

    Initial commit

---
+++ BCJavaSber/src/Main.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static List<City> listCity(File data) throws Exception {
+        Scanner cityData = new Scanner(data);
+        List<City> cityList = new ArrayList<City>();
+        while (cityData.hasNextLine()) {
+            String city = cityData.nextLine();
+            String[] paramCity = city.split(";");
+            if (paramCity.length == 6) {
+                City modelCity = new City(paramCity[1], paramCity[2], paramCity[3], paramCity[4], paramCity[5]);
+                cityList.add(modelCity);
+            } else {
+                City modelCity = new City(paramCity[1], paramCity[2], paramCity[3], paramCity[4]);
+                cityList.add(modelCity);
+            }
+        }
+        return cityList;
+    }
+
+    public static class City {
+        String name;
+        String region;
+        String district;
+        int population;
+        String foundation;
+
+        public City(String name, String region, String district, String population, String foundation) {
+            this.name = name;
+            this.region = region;
+            this.district = district;
+            this.population = Integer.parseInt(population);
+            this.foundation = foundation;
+        }
+
+        public City(String name, String region, String district, String population) {
+            this.name = name;
+            this.region = region;
+            this.district = district;
+            this.population = Integer.parseInt(population);
+
+        }
+
+        public String toString() {
+            return "City{" +
+                    "name='" + name + '\'' +
+                    ", region='" + region + '\'' +
+                    ", district='" + district + '\'' +
+                    ", population=" + population +
+                    ", foundation='" + foundation + '\'' +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        File data = new File("resources/CityData.csv");
+        List<City> city = listCity(data);
+        for (City ct : city) System.out.println(ct);
+
+    }
+}
